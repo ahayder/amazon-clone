f055a84c1661a1353eac77dd28ce23202c468284
ahayderbs <ali.hayder@brainstation23.com>
2020-09-11 19:11:36 +0600

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDvmiyMpXA-9cSsXS1P_0jcEti1xnvwZjA",
+  authDomain: "clone-69f6d.firebaseapp.com",
+  databaseURL: "https://clone-69f6d.firebaseio.com",
+  projectId: "clone-69f6d",
+  storageBucket: "clone-69f6d.appspot.com",
+  messagingSenderId: "232053595099",
+  appId: "1:232053595099:web:adfa91ed29fa32528265fe"
+};
